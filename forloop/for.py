8bce622dfc0d7a98aae005d617d42e683380ceaa
cmpmohan <m.mohan905@gmail.com>
2017-07-25 21:44:12 +0530

    Update for.py

--- forloop/for.py
+++ forloop/for.py
@@ -1,3 +1,26 @@
+
+Write a Python program to construct the following pattern
+
+`*` 
+
+`* *`
+
+`* * *`
+
+`* * * *` 
+
+`* * * * *` 
+
+`* * * *` 
+
+`* * *` 
+
+`* *` 
+
+`*`
+
+Solution:
+
 for i in range(1,6):
  print('*' * i)
 for l in range(1,6):
